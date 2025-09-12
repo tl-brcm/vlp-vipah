6880f4fa9ed51fd91c34368979f8381ec23da823
tl-brcm <tl-brcm@demo.com>
2025-09-12 13:39:13 +0800

    added sample policies

--- vipah_deploy_scripts/6.Configure_ID_Store.sh
+++ vipah_deploy_scripts/6.Configure_ID_Store.sh
@@ -10,6 +10,7 @@ source ${DIRNAME}/0.gks_env.sh
 NC="\033[0m"
 
 function print_color(){
+
   case $1 in
     "green") COLOR="\033[0;32m" ;;
     "red")  COLOR="\033[0;31m" ;;
@@ -28,7 +29,7 @@ print_color "green" "####################################################"
 # First get the tenant ID and from there get the access token to call the rest API for futher confiugrations. 
 TENANT_ADMIN_CLIENTID=$(kubectl get secret ${RELEASENAME}-ssp-secret-defaulttenantclient -n ${NAMESPACE} -o jsonpath="{.data.clientId}" | base64 --decode)
 TENANT_ADMIN_CLIENTSECRET=$(kubectl get secret ${RELEASENAME}-ssp-secret-defaulttenantclient -n ${NAMESPACE} -o jsonpath="{.data.clientSecret}" | base64 --decode)
-SSP_AT=$(curl -s --insecure -u "${TENANT_ADMIN_CLIENTID}:${TENANT_ADMIN_CLIENTSECRET}" --request POST --url "https://${SSP_FQDN}/default/oauth2/v1/token" --header "Content-Type: application/x-www-form-urlencoded" --header "x-tenant-name: default" --data-urlencode "grant_type=client_credentials" --data-urlencode "scope=urn:iam:myscopes" | jq -r .access_token )
+SSP_AT=$(curl -s --insecure -u "${TENANT_ADMIN_CLIENTID}:${TENANT_ADMIN_CLIENTSECRET}" --request POST --url "https://${SSP_FQDN}/default/oauth2/v1/token" --header "Content-Type: application/x-www-form-urlencoded" --data-urlencode "grant_type=client_credentials" --data-urlencode "scope=urn:iam:myscopes" | jq -r .access_token )
 
 print_color "default" ""
 print_color "green" "CORS Setting to Reflect Sample Application's FQDN"
@@ -37,13 +38,39 @@ print_color "default" ""
 # Update the access token based on our domain
 sed 's/\[SA_FQDN\]/'${SA_FQDN}'/g' allow_origin_template.json > allow_origin.json
 
-curl --insecure -L -X PATCH "https://${SSP_FQDN}/default/admin/v1/Configs" -H 'Content-Type: application/json' -H "Authorization: Bearer ${SSP_AT}" -H "x-tenant-name: default" -d '@./allow_origin.json'
+curl --insecure -L -X PATCH "https://${SSP_FQDN}/default/admin/v1/Configs" -H 'Content-Type: application/json' -H "Authorization: Bearer ${SSP_AT}" -d '@./allow_origin.json'
 
 print_color "default" ""
 print_color "green" "Add VIP Certificate for VIP PUSH"
 print_color "default" ""
 
-curl --insecure -L -X POST "https://${SSP_FQDN}/default/admin/v1/VIPConfiguration" -H 'Content-Type: application/json' -H "Authorization: Bearer ${SSP_AT}" -H "x-tenant-name: default" -d '@./vipCert.json'
+curl --insecure -L -X POST "https://${SSP_FQDN}/default/admin/v1/VIPConfiguration" -H 'Content-Type: application/json' -H "Authorization: Bearer ${SSP_AT}" -d '@./vipCert.json'
+
+
+print_color "default" ""
+print_color "green" "LDAP Store Configuration in VIP Auth Hub"
+print_color "default"
+
+
+curl --insecure --request POST "https://${SSP_FQDN}/default/admin/v1/LDAPconfigs" -H 'Content-Type: application/json'  -H "Authorization: Bearer ${SSP_AT}" -d '@./ldapconfig_sample.json'
+
+
+print_color "default" ""
+print_color "green" "Add LDAP Admin Group as VIP Auth Hub Admin Group"
+print_color "green" "When you want to use a different LDAP group name as VIP Admin group, please update AdminAzPolicy_template.json file"
+print_color "default"
+
+POLICY_ID=$(curl -s --insecure --request GET "https://${SSP_FQDN}/default/admin/v1/AuthZPolicies" -H 'Content-Type: application/json'  -H "Authorization: Bearer ${SSP_AT}" | jq -r '.[] | select(.policyName=="TenantAdminPolicy").policyId')
+
+RULE_ID=$(curl -s --insecure --request GET "https://${SSP_FQDN}/default/admin/v1/AuthZPolicies" -H 'Content-Type: application/json'  -H "Authorization: Bearer ${SSP_AT}" | jq -r '.[] | select(.policyName=="TenantAdminPolicy").rules[].id')
+
+sed 's/\[RuleId\]/'${RULE_ID}'/g' AdminAzPolicy_template.json > AdminAzPolicy.json
+
+curl --insecure -L -X PATCH "https://${SSP_FQDN}/default/admin/v1/AuthZPolicies/${POLICY_ID}" -H 'Content-Type: application/json' -H "Authorization: Bearer ${SSP_AT}" -d '@./AdminAzPolicy.json'
+
+kubectl get svc -n ${SA_NAMESPACE}
+
+IPADDRESS="$(kubectl get svc -n ${SA_NAMESPACE} | grep -i LoadBalancer | awk '{ print $4 }' )"
 
 print_color "green" "####################################################"
 print_color "default" ""
@@ -52,3 +79,4 @@ print_color "red" "Complete ID Store setup.. Please access the Sample App and va
 print_color "red" "Sample App URL: https://${SA_FQDN}/sample-app/ "
 print_color "default" ""
 print_color "green" "####################################################"
+
